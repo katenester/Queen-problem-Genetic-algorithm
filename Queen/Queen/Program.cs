26a544cc8a2323379f378c25eab703ac8dcc1f0a
katenester1995 <nesterenkokm@bk.ru>
2023-11-29 18:33:08 +0300

    Добавлены комментарии к коду

--- Queen/Queen/Program.cs
+++ Queen/Queen/Program.cs
@@ -2,68 +2,68 @@
 
 class Program
 {
-    static int populationSize = 8;
-    static int[][] parent = new int[populationSize][];
-    static int[][] children = new int[populationSize][];
-    static int[] parentFitness = new int[populationSize];
-    static int[] childrenFitness = new int[populationSize];
+    static int populationSize = 8; // Количество особей в популяции
+    static int[][] parent = new int[populationSize][]; // Массив родителей (особей)
+    static int[][] children = new int[populationSize][]; // Массив потомков (особей)
+    static int[] parentFitness = new int[populationSize]; // Массив значений приспособленности для родителей
+    static int[] childrenFitness = new int[populationSize]; // Массив значений приспособленности для потомков
 
     static void Main(string[] args)
     {
-        InitialPopulation();
-        int count = 0;
-        double find = double.NaN;
+        InitialPopulation(); // Создание начальной популяции
+        int count = 0; // Счетчик поколений
+        double find = double.NaN; // Переменная для хранения индекса найденной особи
 
-        while (true)
+        while (true) // Начало эволюционного процесса
         {
-            count++;
-            if (count % 1000 == 0)
+            count++; // Увеличение счетчика поколений
+            if (count % 1000 == 0) // Каждые 1000 поколений выводится информация о текущем состоянии
             {
                 Console.WriteLine($"First {count} Subsets");
             }
 
-            for (int k = 0; k < populationSize / 2; k++)
+            for (int k = 0; k < populationSize / 2; k++) // Создание новых потомков путем скрещивания
             {
                 Hybridization();
             }
 
-            for (int k = 0; k < populationSize; k++)
+            for (int k = 0; k < populationSize; k++) // Проверка на достижение оптимального решения
             {
-                if (childrenFitness[k] == 28)
+                if (childrenFitness[k] == 28) // Если значение приспособленности равно 28 (оптимальное решение)
                 {
-                    find = k;
+                    find = k; // Сохранение индекса найденной особи
                     break;
                 }
             }
 
-            if (!double.IsNaN(find))
+            if (!double.IsNaN(find)) // Если найдено оптимальное решение, процесс завершается
             {
                 break;
             }
 
-            Array.Copy(children, parent, populationSize);
-            Array.Copy(childrenFitness, parentFitness, populationSize);
+            Array.Copy(children, parent, populationSize); // Обновление родителей новыми потомками
+            Array.Copy(childrenFitness, parentFitness, populationSize); // Обновление значений приспособленности родителей
 
-            Array.Clear(children, 0, populationSize);
-            Array.Clear(childrenFitness, 0, populationSize);
+            Array.Clear(children, 0, populationSize); // Очистка массива потомков
+            Array.Clear(childrenFitness, 0, populationSize); // Очистка массива значений приспособленности потомков
         }
 
-        int[] res = children[(int)find];
+        int[] res = children[(int)find]; // Найденная оптимальная особь
         Console.WriteLine("Result:");
-        foreach (int item in res)
+        foreach (int item in res) // Вывод найденной оптимальной особи
         {
             Console.Write($"{item} ");
         }
 
         Console.WriteLine("\nFind results:");
-        int[,] resQueen = new int[8, 8];
+        int[,] resQueen = new int[8, 8]; // Доска для отображения решения задачи
 
-        for (int t = 0; t < 8; t++)
+        for (int t = 0; t < 8; t++) // Заполнение доски на основе найденной оптимальной особи
         {
             resQueen[res[t], t] = 1;
         }
 
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++) // Вывод доски с решением задачи
         {
             for (int j = 0; j < 8; j++)
             {
@@ -73,23 +73,23 @@ class Program
         }
     }
 
-    static void InitialIndividual()
+    static void InitialIndividual() // Генерация начальной особи
     {
-        int[] individual = new int[8];
-        Random random = new Random();
+        int[] individual = new int[8]; // Массив для хранения генов особи
+        Random random = new Random(); // Генератор случайных чисел
 
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++) // Заполнение генов случайными значениями
         {
             int a = random.Next(0, 8);
             individual[i] = a;
         }
 
-        int fitScore = UpdateFitnessScore(individual);
-        parentFitness[Array.IndexOf(parentFitness, 0)] = fitScore;
-        parent[Array.IndexOf(parent, null)] = individual;
+        int fitScore = UpdateFitnessScore(individual); // Вычисление значения приспособленности особи
+        parentFitness[Array.IndexOf(parentFitness, 0)] = fitScore; // Сохранение значения приспособленности
+        parent[Array.IndexOf(parent, null)] = individual; // Сохранение особи в массив родителей
     }
 
-    static int UpdateFitnessScore(int[] individual)
+    static int UpdateFitnessScore(int[] individual) // Вычисление значения приспособленности особи
     {
         int value = 0;
 
@@ -110,63 +110,63 @@ class Program
             }
         }
 
-        return value;
+        return value; // Возвращение значения приспособленности
     }
 
-    static void InitialPopulation()
+    static void InitialPopulation() // Создание начальной популяции
     {
         for (int i = 0; i < populationSize; i++)
         {
-            InitialIndividual();
+            InitialIndividual(); // Генерация начальных особей
         }
     }
 
-    static int Select()
+    static int Select() // Выбор особи для скрещивания
     {
         int totalScore = 0;
 
-        foreach (int fit in parentFitness)
+        foreach (int fit in parentFitness) // Вычисление суммы значений приспособленности всех особей
         {
             totalScore += fit;
         }
 
-        Random random = new Random();
-        int num = random.Next(0, totalScore);
+        Random random = new Random(); // Генератор случайных чисел
+        int num = random.Next(0, totalScore); // Случайное число в диапазоне от 0 до суммы значений приспособленности
         int frontScore = 0;
 
-        for (int i = 0; i < populationSize; i++)
+        for (int i = 0; i < populationSize; i++) // Поиск выбранной особи на основе суммы значений приспособленности
         {
             frontScore += parentFitness[i];
 
             if (frontScore >= num)
             {
-                return i;
+                return i; // Возвращение индекса выбранной особи
             }
         }
 
         return 0;
     }
 
-    static int[] Mutation(int[] changeIndividual)
+    static int[] Mutation(int[] changeIndividual) // Мутация особи
     {
-        Random random = new Random();
-        int pos = random.Next(0, 8);
-        int change = random.Next(0, 8);
-        changeIndividual[pos] = change;
+        Random random = new Random(); // Генератор случайных чисел
+        int pos = random.Next(0, 8); // Выбор случайной позиции для мутации
+        int change = random.Next(0, 8); // Новое случайное значение гена для мутации
+        changeIndividual[pos] = change; // Мутация гена
 
-        return changeIndividual;
+        return changeIndividual; // Возвращение измененной особи
     }
 
-    static void Hybridization()
+    static void Hybridization() // Скрещивание особей
     {
-        int first = Select();
-        int second = Select();
-        int[][] selectedParents = new int[2][] { parent[first], parent[second] };
-        Random random = new Random();
-        int pos1 = random.Next(0, 7);
-        int pos2 = random.Next(0, 7);
+        int first = Select(); // Выбор первой особи для скрещивания
+        int second = Select(); // Выбор второй особи для скрещивания
+        int[][] selectedParents = new int[2][] { parent[first], parent[second] }; // Массив выбранных для скрещивания особей
+        Random random = new Random(); // Генератор случайных чисел
+        int pos1 = random.Next(0, 7); // Выбор случайной позиции начала участка скрещивания
+        int pos2 = random.Next(0, 7); // Выбор случайной позиции конца участка скрещивания
 
-        if (pos1 > pos2)
+        if (pos1 > pos2) // Обеспечение правильного порядка позиций участка скрещивания
         {
             int temp = pos1;
             pos1 = pos2;
@@ -174,14 +174,14 @@ class Program
         }
 
         int rangeLength = pos2 - pos1;
-        if (rangeLength >= selectedParents[1].Length) // Check if the range length exceeds the size of the second parent
+        if (rangeLength >= selectedParents[1].Length) // Проверка, не превышает ли длина участка скрещивания размер второй особи
         {
-            pos2 = pos1 + selectedParents[1].Length - 1; // Adjust pos2 to the maximum valid value
+            pos2 = pos1 + selectedParents[1].Length - 1; // Если да, корректировка конечной позиции участка
             rangeLength = pos2 - pos1;
         }
 
-        int[] tmp = new int[rangeLength + 1];
-        for (int i = pos1; i <= pos2; i++)
+        int[] tmp = new int[rangeLength + 1]; // Временный массив для обмена генами между родителями
+        for (int i = pos1; i <= pos2; i++) // Обмен генами между выбранными особями в участке скрещивания
         {
             tmp[i - pos1] = selectedParents[0][i];
             selectedParents[0][i] = selectedParents[1][i];
@@ -189,23 +189,23 @@ class Program
         }
 
         double may = random.NextDouble();
-        if (may > 0.5)
+        if (may > 0.5) // Вероятность мутации первой особи
         {
-            selectedParents[0] = Mutation(selectedParents[0]);
+            selectedParents[0] = Mutation(selectedParents[0]); // Мутация первой особи
         }
 
         may = random.NextDouble();
-        if (may > 0.5)
+        if (may > 0.5) // Вероятность мутации второй особи
         {
-            selectedParents[1] = Mutation(selectedParents[1]);
+            selectedParents[1] = Mutation(selectedParents[1]); // Мутация второй особи
         }
 
-        int firstFit = UpdateFitnessScore(selectedParents[0]);
-        int secondFit = UpdateFitnessScore(selectedParents[1]);
+        int firstFit = UpdateFitnessScore(selectedParents[0]); // Вычисление приспособленности первой потомственной особи
+        int secondFit = UpdateFitnessScore(selectedParents[1]); // Вычисление приспособленности второй потомственной особи
 
-        children[Array.IndexOf(children, null)] = selectedParents[0];
-        children[Array.IndexOf(children, null)] = selectedParents[1];
-        childrenFitness[Array.IndexOf(childrenFitness, 0)] = firstFit;
-        childrenFitness[Array.IndexOf(childrenFitness, 0)] = secondFit;
+        children[Array.IndexOf(children, null)] = selectedParents[0]; // Добавление первой потомственной особи в массив потомков
+        children[Array.IndexOf(children, null)] = selectedParents[1]; // Добавление второй потомственной особи в массив потомков
+        childrenFitness[Array.IndexOf(childrenFitness, 0)] = firstFit; // Сохранение значения приспособленности первой потомственной особи
+        childrenFitness[Array.IndexOf(childrenFitness, 0)] = secondFit; // Сохранение значения приспособленности второй потомственной особи
     }
 }
